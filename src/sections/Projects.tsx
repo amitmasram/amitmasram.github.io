e8ac12697bcaa7589152400ceaab7433b982232c
Amit masram <amitmasram831@gmail.com>
2024-09-20 20:32:28 +0530

    newt

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,11 +1,12 @@
-import tourEase from "@/assets/images/trav.png";
+import tourEase from "@/assets/images/tourease2.png";
 import careDash from "@/assets/images/caredash.png";
-import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import aiStartupLandingPage from "@/assets/images/ai-image.png";
 import Image from "next/image";
 import CheckCircleIcon from "@/assets/icons/check-circle.svg"
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 import { SectionHeader } from "@/components/SectionHeader";
 import { Card } from "@/components/Card";
+import { title } from "process";
 const portfolioProjects = [
   {
     company: "Status",
@@ -36,68 +37,68 @@ const portfolioProjects = [
     year: "Completed",
     title: "AI-Image-Generator",
     results: [
-      { title: "Find skilled professionals for services." },
-      { title: "Connecting users with nearby experts." },
-      { title: "Increased mobile traffic by 35%" },
+      { title: "Built with Flutter for cross-platform use." },
+      { title: "MidJourney API for AI image generation." },
+      { title: "Real-time AI image creation." }
     ],
-    link: "https://github.com/Amitmasram/ProFinder---App",
+    link: "https://github.com/Amitmasram/ai_text_generator",
     image: aiStartupLandingPage,
   },
 ];
 
 export const ProjectsSection = () => {
   return (
-  <section className="pb-16 lg:py-24 bg-black/85">
+    <section className="pb-16 lg:py-24 bg-black/85">
       <div className="container">
-        <SectionHeader eyebrow="Real-world Results" title="Featured Projects" description="See how I transformd concepts into engaging digital expreince."/>
+        <SectionHeader eyebrow="Real-world Results" title="Featured Projects" description="See how I transformd concepts into engaging digital expreince." />
 
 
-           <div className="flex flex-col mt-10 md:mt-20 gap-20">
-            {portfolioProjects.map ((project, projectIndex) =>(
-                  <Card key={project.title} className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky"style={{
-                        top:`calc(64px + ${projectIndex * 40}px)`
-                  }}>
+        <div className="flex flex-col mt-10 md:mt-20 gap-20">
+          {portfolioProjects.map((project, projectIndex) => (
+            <Card key={project.title} className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky" style={{
+              top: `calc(64px + ${projectIndex * 40}px)`
+            }}>
 
 
-                             <div className="lg:grid lg:grid-cols-2 lg:gap-16 ">
-                              <div className="lg:pb-16">
-                              <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16 ">
+                <div className="lg:pb-16">
+                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
 
-                             <span> {project.company}</span>
-                             <span>&bull;</span>
-                             <span>{project.year}</span>
-                             </div>
+                    <span> {project.company}</span>
+                    <span>&bull;</span>
+                    <span>{project.year}</span>
+                  </div>
 
 
-                        <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl ">{project.title}</h3>
-                        <hr className="border-t-2 border-white/5 mt-4  md:mt-5" />
-                        <ul className="flex flex-col gap-4 mt-4 md:mt-5">
-  {project.results.map((result, resultIndex) => (
-    <li key={resultIndex} className="flex gap-2 text-sm md:text-base text-white/50">
-      <CheckCircleIcon className="size-5 md:size-6" />
-      <span>{result.title}</span>
-    </li>
-  ))}
-</ul>
+                  <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl ">{project.title}</h3>
+                  <hr className="border-t-2 border-white/5 mt-4  md:mt-5" />
+                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                    {project.results.map((result, resultIndex) => (
+                      <li key={resultIndex} className="flex gap-2 text-sm md:text-base text-white/50">
+                        <CheckCircleIcon className="size-5 md:size-6" />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
 
-                        <a href={project.link}>
+                  <a href={project.link}>
 
-                        <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex tracking-widest items-center justify-center gap-2 mt-8 -mb-4 md:-mb-0">
-                             <span> Visit Live Site</span>
-                             <ArrowUpRightIcon className="size-4 "/>
+                    <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex tracking-widest items-center justify-center gap-2 mt-8 -mb-4 md:-mb-0">
+                      <span> Visit Live Site</span>
+                      <ArrowUpRightIcon className="size-4 " />
 
-                              </button>
-                              </a>
-                              </div>
-                              <div className="relative">
+                    </button>
+                  </a>
+                </div>
+                <div className="relative">
 
-                        <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"/>
-                        </div>
-             </div>
-                  </Card>
+                  <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none" />
+                </div>
+              </div>
+            </Card>
 
-            ))}
-           </div>
+          ))}
+        </div>
       </div>
-  </section>);
+    </section>);
 };
